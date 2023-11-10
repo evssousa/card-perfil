c23f52f23af4b0df43c993947de3fe782a12d4fd
Everson Sousa <evs.link@hotmail.com>
2023-11-10 17:13:49 -0300

    Criado botão de seguir (usando useState)

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -2,12 +2,25 @@ import styles from './style.module.css'
 import Titulo from '../Titulo'
 import SecaoPerfil from './SecaoPerfil'
 import Botoes from '../Botoes'
+import {useState} from 'react'
 
 export default function Profile({ avatar, nome, bio, email, fone, githubUrl, linkedinUrl, twitterUrl }) {
+
+    // valor, funcao modificadora
+    const [seguir, setSeguir] = useState('Seguir')
+
+    function cliqueBotao(ev) {
+        alert(`Você começou a seguir ${nome}`)
+        setSeguir('Seguindo')
+    }
+
     return (
         <div className={styles.container}>
             <img src={avatar} alt={nome} />
-            <Titulo>{nome}</Titulo>
+            <Titulo>
+                {nome}
+                <button onClick={cliqueBotao}>{seguir}</button>
+            </Titulo>
             <SecaoPerfil>{bio}</SecaoPerfil>
             <SecaoPerfil>{fone}</SecaoPerfil>
             <SecaoPerfil>{email}</SecaoPerfil>
